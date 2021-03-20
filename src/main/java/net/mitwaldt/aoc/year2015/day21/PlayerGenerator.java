b88740962a4cb91a375ddf0d1d54f369a7488cdb
mmirwaldt <michael.mirwaldt@gmail.com>
2021-03-20 01:20:25 +0100

    improved code and solved part two of the puzzle

--- src/main/java/net/mitwaldt/aoc/year2015/day21/PlayerGenerator.java
+++ src/main/java/net/mitwaldt/aoc/year2015/day21/PlayerGenerator.java
@@ -3,23 +3,23 @@ package net.mitwaldt.aoc.year2015.day21;
 import java.util.*;
 
 public class PlayerGenerator {
-    public static List<Player> createPlayers(int hitPoints) {
+    public static List<Player> createPlayers() {
         final List<Player> players = new ArrayList<>();
         for (Weapon weapon : Weapon.values()) {
             for (Armor armor : Armor.values()) {
                 for (int numberOfRings = 0; numberOfRings <= 2; numberOfRings++) {
                     if(numberOfRings == 0) {
-                        players.add(new Player("me", hitPoints, weapon, armor, Collections.emptyList()));
+                        players.add(new Player("me", 100, weapon, armor, Collections.emptyList()));
                     }
                     for (int oneRingIndex = 0; numberOfRings == 1 && oneRingIndex < Ring.values().length; oneRingIndex++) {
                         final Ring ring = Ring.values()[oneRingIndex];
-                        players.add(new Player("me", hitPoints, weapon, armor, Collections.singletonList(ring)));
+                        players.add(new Player("me", 100, weapon, armor, Collections.singletonList(ring)));
                     }
                     for (int firstRingIndex = 0; numberOfRings == 2 && firstRingIndex < Ring.values().length - 1; firstRingIndex++) {
                         for (int secondRingIndex = firstRingIndex + 1; secondRingIndex < Ring.values().length; secondRingIndex++) {
                             final Ring firstRing = Ring.values()[firstRingIndex];
                             final Ring secondRing = Ring.values()[secondRingIndex];
-                            players.add(new Player("me", hitPoints, weapon, armor, Arrays.asList(firstRing, secondRing)));
+                            players.add(new Player("me", 100, weapon, armor, Arrays.asList(firstRing, secondRing)));
                         }
                     }
                 }
--- src/main/java/net/mitwaldt/aoc/year2015/day21/RpgSimulatorMain.java
+++ src/main/java/net/mitwaldt/aoc/year2015/day21/RpgSimulatorMain.java
@@ -2,23 +2,28 @@ package net.mitwaldt.aoc.year2015.day21;
 
 import java.util.List;
 
+import static java.lang.Math.max;
 import static java.lang.Math.min;
 
 public class RpgSimulatorMain {
     public static void main(String[] args) {
-        // I am too lazy to read out the values from input.txt
-        final List<Player> mePlayers = PlayerGenerator.createPlayers(100);
+        final List<Player> mePlayers = PlayerGenerator.createPlayers();
         int minCost = Integer.MAX_VALUE;
+        int maxCost = 0;
         for (Player me : mePlayers) {
             final Player winner = RpgSimulator.fight(me, createEnemy());
             if(winner == me) {
-                minCost = min(minCost, winner.getCost());
+                minCost = min(minCost, me.getCost()); // for part one
+            } else {
+                maxCost = max(maxCost, me.getCost()); // for part two
             }
         }
         System.out.println(minCost); // result : 78
+        System.out.println(maxCost); // result : 148
     }
 
     private static Player createEnemy() {
+        // I am too lazy to read out the values from input.txt
         return new Player("enemy", 104, 8, 1);
     }
 }
